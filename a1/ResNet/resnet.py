85a465c5d2fb833e5ed23bdaa2f5f2e797c5d8df
Aniruddha Deb <aniruddha.deb.2002@gmail.com>
2023-03-25 12:33:53 +0530

    New stuff

--- a1/ResNet/resnet.py
+++ a1/ResNet/resnet.py
@@ -4,32 +4,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-from torch.cuda import is_available
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from tqdm import tqdm
+import copy
+import torchmetrics.functional as tmf
 
 import pickle
 
 from torch.utils.data import Dataset, DataLoader, Subset
 
-"""
-PARAMETERS
-"""
-
-lr = 1e-5
-dpath = 'data'
-
-splits = {
-    'train': [f'{dpath}/data_batch_{i}' for i in range(1,5)],
-    'val'  : [f'{dpath}/data_batch_5']
-}
-
 DEBUG = False
 debug_len = {
-    'train': 128,
-    'val': 32
+    'train': 512,
+    'val': 128
 }
 
 class Cifar10Dataset(Dataset):
@@ -66,23 +55,36 @@ class Cifar10Dataset(Dataset):
 # stores the parameters, and subclasses which use it's buffers
 class Normalizer(nn.Module):
 
-    def __init__(self, num_features):
+    # num_features: C
+    # although this parameter is unused for channel-invariant filters (eg LayerNormalizer)
+    def __init__(self, num_features, track_running_stats=False):
         super().__init__()
         self.weight = nn.Parameter(torch.ones((num_features, 1)))
         self.bias = nn.Parameter(torch.zeros((num_features, 1)))
+        self.eps = 1e-5
+        self.momentum = 0.1
 
-        self.running_mean = nn.Parameter(torch.zeros((num_features, 1)), requires_grad=False)
-        self.running_var = nn.Parameter(torch.ones((num_features, 1)), requires_grad=False)
-        self.num_batches_tracked = nn.Parameter(torch.tensor(0), requires_grad=False)
+        self.tracking_running_stats = track_running_stats
+        if self.tracking_running_stats:
+            self.running_mean = nn.Parameter(torch.zeros((num_features, 1)), requires_grad=False)
+            self.running_var = nn.Parameter(torch.ones((num_features, 1)), requires_grad=False)
+            self.num_batches_tracked = nn.Parameter(torch.tensor(0), requires_grad=False)
 
     def reset_stats(self):
-        self.running_mean.zero_()
-        self.running_var.fill_(1)
-        self.num_batches_tracked.zero_()
+        if self.tracking_running_stats:
+            self.running_mean.zero_()
+            self.running_var.fill_(1)
+            self.num_batches_tracked.zero_()
 
     def reset_params(self):
         self.weight.fill_(1)
         self.bias.zero_()
+
+    def update_stats(self, mean, var):
+        if self.tracking_running_stats:
+            self.running_mean.data = self.momentum*mean + (1-self.momentum)*self.running_mean.data
+            self.running_var.data = self.momentum*var + (1-self.momentum)*self.running_var.data
+            self.num_batches_tracked += 1
 
     def dim_check(self, x, tgt_dim):
         if (x.dim() != tgt_dim):
@@ -90,150 +92,136 @@ class Normalizer(nn.Module):
 
 class BatchNormalizer(Normalizer):
 
-    def __init__(self, num_features):
-        super().__init__(num_features)
+    def __init__(self, num_features, track_running_stats=True):
+        super().__init__(num_features, track_running_stats=track_running_stats)
+
+    def _transform(self, x):
+        x = x.flatten(start_dim=2, end_dim=3)
+        # x : (B, C, H*W)
+
+        if self.tracking_running_stats and not self.training:
+            mean = self.running_mean
+            var = self.running_var
+        else:
+            mean = x.mean((0,2)).unsqueeze(1) # over all pixels and examples (one mean per channel)
+            var = x.var((0,2), unbiased=False).unsqueeze(1)
+            self.update_stats(mean, var)
+
+        return (x - mean)/torch.sqrt(var + self.eps)
 
     def forward(self, x):
         # x : (B, C, H, W)
         self.dim_check(x, 4)
         b, c, h, w = x.shape
+        x_hat = self._transform(x)
+
+        norm_x = self.weight * x_hat + self.bias
+
+        return norm_x.unflatten(2, (h,w))
+
+class InstanceNormalizer(Normalizer):
+
+    def __init__(self, num_features, track_running_stats=False):
+        super().__init__(num_features, track_running_stats=track_running_stats)
+
+    def _transform(self, x):
+        # x : (B, C, H, W)
         x = x.flatten(start_dim=2, end_dim=3)
         # x : (B, C, H*W)
 
-        if self.training:
-            mean = x.mean((0,2)).unsqueeze(1) # over all pixels and examples (one mean per channel)
-            var = x.var((0,2), unbiased=False).unsqueeze(1)
-
-            # n = self.num_batches_tracked
-            # n_new = self.num_batches_tracked+1
-            # EMA with momentum of 0.1
-            self.running_mean.data = 0.1*mean + 0.9*self.running_mean.data #((self.running_mean.data)/n_new)*n + mean/n_new
-            self.running_var.data = 0.1*var + 0.9*self.running_var.data #((self.running_var.data)/(n_new*n_new))*(n*n) + var/(n_new*n_new)
-            self.num_batches_tracked += 1
-        else:
+        if self.tracking_running_stats and not self.training:
             mean = self.running_mean
             var = self.running_var
+        else:
+            mean = x.mean(2).unsqueeze(2) # over all pixels and examples (one mean per channel)
+            var = x.var(2, unbiased=False).unsqueeze(2)
+            self.update_stats(mean, var)
 
-        # print(f'x_max: {x.max():.3f}, x_min: {x.min():.3f}')
-        # print(f'mean_max: {mean.max():.3f}, mean_min: {mean.min():.3f}')
-        # print(f'var_max: {var.max():.3f}, var_min: {var.min():.3f}')
-        norm_x = self.weight * (x - mean)/(torch.sqrt(var + 1e-5)) + self.bias
-        # print(f'norm_x_max: {norm_x.max():.3f}, norm_x_min: {norm_x.min():.3f}')
+        return (x - mean)/torch.sqrt(var + self.eps)
+
+    def forward(self, x):
+        self.dim_check(x, 4)
+        b, c, h, w = x.shape
+
+        x_hat = self._transform(x)
+        norm_x = self.weight * x_hat + self.bias
 
         return norm_x.unflatten(2, (h,w))
+
+class BatchInstanceNormalizer(nn.Module):
+
+    def __init__(self, num_features):
+        super().__init__()
+        self.rho = nn.Parameter(torch.tensor([0.5]))
+
+        # BIN uses a BatchNormalizer that tracks running statistics
+        self.bnorm = BatchNormalizer(num_features)
+        self.inorm = InstanceNormalizer(num_features)
+
+        self.weight = nn.Parameter(torch.ones((num_features, 1)))
+        self.bias = nn.Parameter(torch.zeros((num_features, 1)))
+
+    def forward(self, x):
+        # x : (B, C, H, W)
+        self.bnorm.dim_check(x, 4)
+        b, c, h, w = x.shape
+        x_bn = self.bnorm._transform(x)
+        x_in = self.inorm._transform(x)
+
+        x_bin = self.weight * (self.rho * x_bn + (1 - self.rho) * x_in) + self.bias
+
+        return x_bin.unflatten(2, (h,w))
 
 class LayerNormalizer(Normalizer):
 
     def __init__(self, num_features):
-        super().__init__(num_features)
+        super().__init__(1)
 
-    def forward(self, x):
+    def _transform(self, x):
         # x : (B, C, H, W)
-        self.dim_check(x, 4)
-        b, c, h, w = x.shape
         x = x.flatten(start_dim=1, end_dim=3)
         # x : (B, C*H*W)
 
-        if self.training:
-            mean = x.mean(1) 
-            var = x.var(1) 
+        # layer norm uses the current mean and variance and doesn't memorize
+        # old means and variances.
+        mean = x.mean(1).unsqueeze(1) # over all examples in batch
+        var = x.var(1, unbiased=False).unsqueeze(1)
 
-            self.running_mean = ((self.running_mean)/(self.num_batches_tracked+1))*self.num_batches_tracked + \
-                    mean/(self.num_batches_tracked+1)
-            self.running_var = ((self.running_var)/(self.num_batches_tracked+1))*self.num_batches_tracked + \
-                    var/(self.num_batches_tracked+1)
-            self.num_batches_tracked += 1
-        else:
-            mean = self.running_mean
-            var = self.running_var
-
-        norm_x = self.weight * (x - mean.unsqueeze(1))/(torch.sqrt(var.unsqueeze(1) + 1e-5)) + self.bias
-
-        return norm_x.unflatten(1, (c,h,w))
-    
-class InstanceNormalizer(Normalizer):
-
-    def __init__(self, num_features):
-        super().__init__(num_features)
+        return (x - mean)/torch.sqrt(var + self.eps)
 
     def forward(self, x):
-        # x : (B, C, H, W)
         self.dim_check(x, 4)
         b, c, h, w = x.shape
-        x = x.flatten(start_dim=2, end_dim=3)
-        # x : (B, C, H*W)
 
-        if self.training:
-            mean = x.mean(2)
-            var = x.var(2) 
+        x_hat = self._transform(x)
+        norm_x = self.weight * x_hat + self.bias
 
-            self.running_mean = ((self.running_mean)/(self.num_batches_tracked+1))*self.num_batches_tracked + \
-                    mean/(self.num_batches_tracked+1)
-            self.running_var = ((self.running_var)/(self.num_batches_tracked+1))*self.num_batches_tracked + \
-                    var/(self.num_batches_tracked+1)
-            self.num_batches_tracked += 1
-        else:
-            mean = self.running_mean
-            var = self.running_var
-
-        norm_x = self.weight * (x - mean.unsqueeze(1))/(torch.sqrt(var.unsqueeze(1) + 1e-5)) + self.bias
-
-        return norm_x.unflatten(2, (h,w))
-
-# # TODO
-# class BatchInstanceNormalizer(Normalizer):
-# 
-#     def forward(self, x):
-#         self.dim_check(x, 4)
-#         
-#         out_bn = normalize(
-#                 x,
-#                 [0], # normalize across axis 0 (batches)
-#                 self.running_mean,
-#                 self.running_var,
-#                 self.weight,
-#                 self.bias,
-#                 self.training)
-#         
-#         return out_bn
+        return norm_x.unflatten(1, (c,h,w))
 
 class GroupNormalizer(Normalizer):
 
     def __init__(self, num_features, num_groups=16):
-        super().__init__(num_features)
+        super().__init__(num_groups)
         self.num_groups = num_groups
 
     def forward(self, x):
         # x : (B, C, H, W)
         self.dim_check(x, 4)
         b, c, h, w = x.shape
-        # this implementation requires that the number of channels is an even
-        # multiple of the number of groups
-        g = c/self.num_groups
+        g = c//self.num_groups
         x = x.reshape((b,self.num_groups, g*h*w))
         # x : (B, n_g, G*H*W)
 
-        if self.training:
-            mean = x.mean(2)
-            var = x.var(2) 
+        mean = x.mean(2).unsqueeze(2) # over all groups
+        var = x.var(2, unbiased=False).unsqueeze(2)
 
-            self.running_mean = ((self.running_mean)/(self.num_batches_tracked+1))*self.num_batches_tracked + \
-                    mean/(self.num_batches_tracked+1)
-            self.running_var = ((self.running_var)/(self.num_batches_tracked+1))*self.num_batches_tracked + \
-                    var/(self.num_batches_tracked+1)
-            self.num_batches_tracked += 1
-        else:
-            mean = self.running_mean
-            var = self.running_var
-
-        norm_x = self.weight * (x - mean.unsqueeze(1))/(torch.sqrt(var.unsqueeze(1) + 1e-5)) + self.bias
+        norm_x = self.weight * (x - mean)/(torch.sqrt(var + self.eps)) + self.bias
 
         return norm_x.reshape((b,c,h,w))
 
 class ResNetBlock(nn.Module):
     
     def __init__(self, in_channels, out_channels, batch_norm_class=nn.BatchNorm2d, downsample=False, in_dim=32*32):
-        # TODO add ReLU, BatchNorm etc
         super().__init__()
         
         self.downsample = downsample
@@ -275,10 +263,10 @@ class ResNet(nn.Module):
         
         self.network = nn.Sequential(
             nn.Conv2d(3,16,3),
-            nn.BatchNorm2d(16),
+            batch_norm_class(16),
             nn.ReLU(),
             nn.Sequential(*[ResNetBlock(16,16,batch_norm_class=batch_norm_class) for _ in range(n)]),
-            ResNetBlock(16,32,batch_norm_class=batch_norm_class,downsample=True,in_dim=32*32),
+            ResNetBlock(16,32,downsample=True,in_dim=32*32),
             nn.Sequential(*[ResNetBlock(32,32,batch_norm_class=batch_norm_class) for _ in range(n-1)]),
             ResNetBlock(32,64,batch_norm_class=batch_norm_class,downsample=True,in_dim=16*16),
             nn.Sequential(*[ResNetBlock(64,64,batch_norm_class=batch_norm_class) for _ in range(n-1)]),
@@ -290,29 +278,14 @@ class ResNet(nn.Module):
     def forward(self, x):
         return self.network(x)
 
-if __name__ == "__main__":
-    datasets = {split: Cifar10Dataset(batch_files) for split, batch_files in splits.items()}
-
-    device = torch.device('cpu')
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
-    if torch.backends.mps.is_available():
-        device = torch.device('mps')
-    
-    if DEBUG:
-        datasets = {split: Subset(dataset, np.arange(debug_len[split])) for split, dataset in datasets.items()}
-    
-    dataloaders = {split: DataLoader(dataset, batch_size=128, num_workers=4, shuffle=True) for split, dataset in datasets.items()}
-    
-    model = ResNet(2, 10, batch_norm_class=BatchNormalizer).to(device)
-    
-    optimizer = optim.SGD(model.parameters(), lr=0.1)
-    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
-    loss_fn = nn.CrossEntropyLoss()
-    
+def train_model(model, optimizer, scheduler, dataloaders, loss_fn, 
+                max_epochs=100, early_stopping=True, max_patience=5):
     train_losses = []
     val_losses = []
-    for i in tqdm(range(5)):
+    best_val_loss = 10000
+    patience = 0
+    best_model = None
+    for i in tqdm(range(max_epochs)):
         
         curr_train_loss = 0
         curr_val_loss = 0
@@ -333,29 +306,136 @@ if __name__ == "__main__":
             
             curr_train_loss += loss.detach().item()
 
-        scheduler.step()
-        
-        model.eval()
-        with torch.no_grad():
-            # validation
-            for images, labels in dataloaders['val']:
-                images = images.to(device)
-                labels = labels.to(device)
-                outputs = model(images)
-                loss = loss_fn(outputs, labels)
-                curr_val_loss += loss.detach().item()
-            
         curr_train_loss /= len(dataloaders['train'])
-        curr_val_loss /= len(dataloaders['val'])
-        
         train_losses.append(curr_train_loss)
-        val_losses.append(curr_val_loss)
+        if scheduler:
+            scheduler.step()
         
-        # if (i%5 == 0):
-        print(f"Epoch {i}:")
-        print(f"    Train loss : {curr_train_loss}")
-        print(f"    Val loss   : {curr_val_loss}")
+        if 'val' in dataloaders:
+            model.eval()
+            with torch.no_grad():
+                # validation
+                for images, labels in dataloaders['val']:
+                    images = images.to(device)
+                    labels = labels.to(device)
+                    outputs = model(images)
+                    loss = loss_fn(outputs, labels)
+                    curr_val_loss += loss.detach().item()
+            curr_val_loss /= len(dataloaders['val'])
+            val_losses.append(curr_val_loss)
+
+            if early_stopping:
+                if curr_val_loss >= best_val_loss:
+                    patience += 1
+                    if patience >= max_patience:
+                        print(f"Early stopping at epoch {i+1}")
+                        break
+                else:
+                    patience = 0 
+                    best_val_loss = curr_val_loss
+                    best_model = copy.deepcopy(model)
+        
+        if (i%5 == 0):
+            print(f"Epoch {i+1}:")
+            print(f"    Train loss : {curr_train_loss}")
+            if 'val' in dataloaders:
+                print(f"    Val loss   : {curr_val_loss}")
+                # print(f"    Patience   : {patience}")
     
-    plt.plot(train_losses)
-    plt.plot(val_losses)
+    return best_model, (train_losses, val_losses)
+
+def eval_model(model, dataloaders):
+
+    print("Evaluating...")
+    model.eval()
+    for dl_name, dl in dataloaders.items():
+        all_preds = []
+        all_true = []
+        for images, labels in tqdm(dl):
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            preds = torch.argmax(outputs, dim=1)
+            all_preds.append(preds)
+            all_true.append(labels)
+
+        all_preds = torch.hstack(all_preds).cpu()
+        all_true = torch.hstack(all_true).cpu()
+
+        acc = tmf.accuracy(all_preds, all_true, task='multiclass', num_classes=10)
+        f1m = tmf.f1_score(all_preds, all_true, task='multiclass', num_classes=10, average='micro')
+        f1M = tmf.f1_score(all_preds, all_true, task='multiclass', num_classes=10, average='macro')
+
+        print(f'{dl_name}:')
+        print(f'    Accuracy : {acc:.3f}')
+        print(f'    F1 micro : {f1m:.3f}')
+        print(f'    F1 macro : {f1M:.3f}')
+
+def q1_part2(cifar10_path):
+    splits = {'train': [f'{cifar10_path}/data_batch_{i}' for i in range(1,6)]}
+    datasets = {split: Cifar10Dataset(batch_files) for split, batch_files in splits.items()}
+
+    n_epochs = 100
+    if DEBUG:
+        datasets = {split: Subset(dataset, np.arange(debug_len[split])) for split, dataset in datasets.items()}
+        n_epochs = 5
+
+    dataloaders = {split: DataLoader(dataset, batch_size=128, num_workers=4, shuffle=True) for split, dataset in datasets.items()}
+    model = ResNet(2, 10).to(device)
+
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
+    loss_fn = nn.CrossEntropyLoss()
+
+    best_model, (train_losses, val_losses) = train_model(model, optimizer, scheduler, dataloaders, loss_fn, max_epochs=n_epochs, early_stopping=False)
+
+    fig, ax = plt.subplots(figsize=(12,8))
+    ax.plot(range(1,n_epochs+1), train_losses)
+    ax.set_title(f'ResNet (n=2, {n_epochs} epochs)')
+    fig.savefig('q1_part2.pdf')
+
+def q1_part3(cifar10_path):
+    splits = {
+        'train': [f'{cifar10_path}/data_batch_{i}' for i in range(1,5)],
+        'val': [f'{cifar10_path}/data_batch_5'],
+        'test': [f'{cifar10_path}/test_batch']
+        }
+    datasets = {split: Cifar10Dataset(batch_files) for split, batch_files in splits.items()}
+
+    n_epochs = 50
+    if DEBUG:
+        datasets = {split: Subset(dataset, np.arange(debug_len[split])) for split, dataset in datasets.items()}
+        n_epochs = 5
+
+    dataloaders = {split: DataLoader(dataset, batch_size=128, num_workers=4, shuffle=True) for split, dataset in datasets.items()}
+    model = ResNet(2, 10).to(device)
+
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.2)
+    loss_fn = nn.CrossEntropyLoss()
+
+    best_model, (train_losses, val_losses) = train_model(model, optimizer, scheduler, dataloaders, loss_fn, max_epochs=n_epochs, early_stopping=True)
+
+    fig, ax = plt.subplots(figsize=(12,8))
+    ax.plot(range(1,len(train_losses)+1), train_losses, label='train')
+    ax.plot(range(1,len(train_losses)+1), val_losses, label='val')
+    ax.legend()
+    ax.set_title(f'ResNet (n=2, {len(train_losses)} epochs, early stopping)')
+    fig.savefig('q1_part3.pdf')
+
+    eval_model(model, dataloaders)
+    torch.save(model, 'part_1.1.pth')
+
+if __name__ == "__main__":
+
+    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+    if torch.backends.mps.is_available():
+        device = torch.device('mps')
+    
+    #q1_part2(cifar10_path='data')
+    q1_part3(cifar10_path='data')
+    
 
